2aec1f07a7f9d2ebf4d05a173560252d12939c39
john.wright <john.wright@workday.com>
2018-09-10 18:02:40 -0700

    support serverless remove

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # serverless-create-global-dynamodb-table
 [![serverless](http://public.serverless.com/badges/v3.svg)](http://www.serverless.com)
 
-A [serverless](http://www.serverless.com) plugin to _automatically_ creates dynamodb global table(s).
+A [serverless](http://www.serverless.com) plugin to _automatically_ creates and cleans up dynamodb global table(s).
 The plugin will create the dynamodb table in the specified region(s) and setup sync between  primary and other table(s).
 
 ## Install
@@ -17,17 +17,20 @@ plugins:
 
 ## Configuration
 
+
 ```yaml
 custom:
   globalTables:
   - regions: # list of regions in which you want to set up global tables
       - region-1
       - region-2
-    tableKey: 'TABLE_KEY' # Cloudformation output key name if the table is created as part of same serverless service
-    tableName: 'TABLE_NAME' # if table is not part of the service then specify the table name. If tableKey param exists then tableName is ignored.
+    tableName: 'TABLE_NAME' # *RECOMMENDED* if table is not part of the service then specify the table name. If tableKey param exists then tableName is ignored.
+    tableKey: 'TABLE_KEY' # Cloudformation output key name if the table is created as part of same serverless service. does not support resource removal
     tags: # List of tags that needs to applied to the new table (optional)
       - Key: tag-key
         Value: tag-value
       - Key: tag-key-2
         Value: tag-value-2
 ```
+
+**Global table cleanup is currently only supported using tableName configuration
--- index.js
+++ index.js
@@ -17,13 +17,12 @@ const createAndTagTable = async function createAndTagTable(region, tableName, se
   })
   try {
     const createResp = await dynamodb.createTable(setting).promise()
-    cli.consoleLog(`CreateGlobalTable: ${chalk.yellow(`Created new table ${tableName} in ${region} region...`)}`);
-    const { TableArn } = createResp.TableDescription;
+    cli.consoleLog(`CreateGlobalTable: ${chalk.yellow(`Created new table ${tableName} in ${region} region...`)}`)
+    const { TableArn } = createResp.TableDescription
     if (tags) {
-      cli.consoleLog(`CreateGlobalTable: ${chalk.yellow(`CAdding tags to table ${tableName} in ${region} region...`)}`);
-      await dynamodb.tagResource({ ResourceArn: TableArn, Tags: tags });
+      cli.consoleLog(`CreateGlobalTable: ${chalk.yellow(`CAdding tags to table ${tableName} in ${region} region...`)}`)
+      await dynamodb.tagResource({ ResourceArn: TableArn, Tags: tags })
     }
-
   } catch (error) {
     if (error.code === 'ResourceInUseException') {
       cli.consoleLog(`CreateGlobalTable: ${chalk.yellow(`Table ${tableName} already exists in the region ${region}`)}`)
@@ -57,6 +56,7 @@ const createGlobalTable = async function createGlobalTable(
   })
   try {
     await dynamodb.describeGlobalTable({ GlobalTableName: tableName }).promise()
+    // TODO should verify that the replica regions are added. if not, attempt to create and add them
     cli.consoleLog(`CreateGlobalTable: ${chalk.yellow('Global table already exists. Skipping creation...')}`)
     return
   } catch (e) {
@@ -91,6 +91,43 @@ const createGlobalTable = async function createGlobalTable(
   }
   await dynamodb.createGlobalTable(param).promise()
   cli.consoleLog(`CreateGlobalTable: ${chalk.yellow(`Created global table setup for ${tableName}...`)}`)
+}
+
+const deleteBackupRegionGlobalTable = async function deleteTable(tableName, backupRegion, creds, cli) {
+  const dynamodb = new AWS.DynamoDB({
+    credentials: creds,
+    region: backupRegion,
+  })
+  try {
+    await dynamodb.deleteTable({ TableName: tableName }).promise()
+    cli.consoleLog(`DeleteGlobalTable: ${chalk.yellow(`Deleted backup region table ${backupRegion} table ${tableName}`)}`)
+  } catch (e) {
+    if (e.code === 'ResourceNotFoundException') {
+      cli.consoleLog(`DeleteGlobalTable: ${chalk.yellow(`Skipping ${backupRegion} for table ${tableName}. The table is already deleted`)}`)
+    } else {
+      cli.consoleLog(`DeleteGlobalTable: ${chalk.yellow(`Unable to delete region ${backupRegion}'s global table ${tableName}. Delete table error code: ${e.code} message: ${e.message}`)}`)
+      throw e
+    }
+  }
+}
+
+/**
+ * This method removes the global table in new region
+ * Attempts to delete the global table and all associated backup tables
+ *
+ * @param {Object} creds AWS temporary credentials
+ * @param {string} tableName primary table name
+ * @param {Array} backupRegions backup regions that need their tables removed
+ * @param {Object} cli Serverless wrapper to write console logs.
+ */
+const deleteBackupRegionGlobalTables = async function deleteGlobalTable(
+  creds,
+  tableName,
+  backupRegions,
+  cli,
+) {
+  await Promise.all(backupRegions.map(backupRegion => deleteBackupRegionGlobalTable(tableName, backupRegion, creds, cli)))
+  return true
 }
 
 /**
@@ -156,6 +193,36 @@ const createGlobalDynamodbTable = async function createGlobalDynamodbTable(serve
   }
 }
 
+const deleteGlobalAndBackupTables = async function deleteGlobalAndBackupTables(serverless) {
+  try {
+    serverless.cli.consoleLog(`DeleteGlobalTable: ${chalk.yellow('Starting to delete backup region global tables...')}`)
+    const awsCredentials = serverless.getProvider('aws').getCredentials()
+
+    const globalTablesOptions = serverless.service.custom.globalTables
+    if (!globalTablesOptions || globalTablesOptions.length === 0) {
+      return
+    }
+    await Promise.all(globalTablesOptions.map((option) => {
+      if (option.tableKey) {
+        serverless.cli.consoleLog(`DeleteGlobalTable: ${chalk.yellow('Warning: backup region global tables not removed. ' +
+          'tableKey serverless.yml option not supported for removal. Switch to tableName for this cleanup to work')}`)
+        return false
+      } else if (option.tableName) {
+        return deleteBackupRegionGlobalTables(
+          awsCredentials.credentials,
+          option.tableName,
+          option.regions,
+          serverless.cli,
+        )
+      } else {
+        throw new Error('Table key or table name is required for removing global tables.')
+      }
+    }))
+  } catch (error) {
+    serverless.cli.consoleLog(`DeleteGlobalTable: ${chalk.yellow(`Failed to delete backup region global tables. Error ${error.message || error}`)}`)
+  }
+}
+
 /**
  * The class that will be used as serverless plugin.
  */
@@ -166,6 +233,9 @@ class CreateGlobalDynamodbTable {
     this.hooks = {
       'after:deploy:deploy': function () {
         createGlobalDynamodbTable(serverless)
+      },
+      'remove:remove': function () {
+        deleteGlobalAndBackupTables(serverless)
       },
     }
   }
